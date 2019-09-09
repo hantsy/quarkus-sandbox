f083fe83b2dda269bc18dfc7f2f90613777a97af
hantsy <hantsy@gmail.com>
2019-09-09 09:54:30 +0800

    remove blog-service code.

--- blog-service/src/main/java/com/example/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String id) {
-        super("Post:" + id + " was not found!");
-    }
-}
