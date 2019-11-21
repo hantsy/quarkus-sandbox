18414b5a8ead79ee2b76e018299fa7071d95886d
hantsy <hantsy@gmail.com>
2019-11-21 10:11:35 +0800

    clean the test codes.

--- spring-post-service/src/test/java/com/example/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- spring-post-service/src/test/java/com/example/PostResourceTest.java
+++ spring-post-service/src/test/java/com/example/PostResourceTest.java
@@ -7,10 +7,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class PostResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testPostsEndpoint() {
         given()
           .when().get("/posts")
           .then()
