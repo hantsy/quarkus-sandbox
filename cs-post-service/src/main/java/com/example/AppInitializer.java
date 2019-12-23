cde7f9239783ae8ddfd9c41c4fb391edb68b82b5
hantsy <hantsy@gmail.com>
2019-12-23 12:39:45 +0800

    initialize the database from init.sql

--- cs-post-service/src/main/java/com/example/AppInitializer.java
+++ cs-post-service/src/main/java/com/example/AppInitializer.java
@@ -9,6 +9,11 @@ import io.vertx.axle.sqlclient.Tuple;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -26,18 +31,28 @@ public class AppInitializer {
         Tuple first = Tuple.of("Hello Quarkus", "My first post of Quarkus");
         Tuple second = Tuple.of("Hello Again, Quarkus", "My second post of Quarkus");
 
-        client.query("CREATE EXTENSION IF NOT EXISTS pgcrypto;\n" +
-                "CREATE TABLE IF NOT EXISTS posts(\n" +
-                "id UUID PRIMARY KEY DEFAULT gen_random_uuid() ,\n" +
-                "title VARCHAR(255) NOT NULL,\n" +
-                "content VARCHAR(255) NOT NULL\n" +
-                ");")
-                .thenCompose(r-> client.query("DELETE FROM posts"))
+        String initScript = readInitScript();
+
+        LOGGER.info("initialized script::" + initScript);
+
+        client.query(initScript)
+                .thenCompose(r -> client.query("DELETE FROM posts"))
                 .thenCompose(r -> client.preparedBatch("INSERT INTO posts (title, content) VALUES ($1, $2)", List.of(first, second)))
                 .thenCompose(r -> client.query("SELECT * FROM posts"))
                 .toCompletableFuture()
                 .join();
 
+    }
+
+    private String readInitScript() {
+        try {
+            return Files.readString(Paths.get(this.getClass().getResource("/init.sql").toURI()), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     void onStop(@Observes ShutdownEvent ev) {
