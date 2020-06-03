0e0c391e48cc322f76de75a9adf2f8e08fe5c2da
hantsy <hantsy@gmail.com>
2020-06-03 19:41:52 +0800

    add jackson object mapper back in Spring using quarkus APIs

--- spring/src/main/java/com/example/AppConfig.java
+++ spring/src/main/java/com/example/AppConfig.java
@@ -1,25 +1,35 @@
 package com.example;
 
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import io.quarkus.jackson.ObjectMapperCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
-import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class AppConfig {
 
-//    @Bean
-//    public ObjectMapper jackson2ObjectMapperBuilder(){
-//        Jackson2ObjectMapperBuilder builder = Jackson2ObjectMapperBuilder.json()
-//                .featuresToEnable(INDENT_OUTPUT)
-//                .featuresToDisable(WRITE_DATES_AS_TIMESTAMPS);
-//
-//        return builder.build();
-//    }
+    @Bean
+    public ObjectMapperCustomizer objectMapperCustomizer() {
+        return (ObjectMapper objectMapper) -> {
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            objectMapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+            objectMapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
 
+            JavaTimeModule module = new JavaTimeModule();
+            LocalDateTimeDeserializer localDateTimeDeserializer = new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            module.addDeserializer(LocalDateTime.class, localDateTimeDeserializer);
+            objectMapper.registerModule(module);
+        };
+    }
 
 }
