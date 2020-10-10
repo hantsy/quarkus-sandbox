940de4bb91b04ae71406ad084c087017ba85602b
hantsy <hantsy@gmail.com>
2020-10-10 12:41:36 +0800

    update to 1.8.3.Final

--- java8/src/test/java/com/example/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
