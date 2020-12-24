bfe1bbc99ef28f3dc8ca3c55a75b3763c1757cad
Hantsy Bai <hantsy@gmail.com>
2020-12-24 13:33:40 +0800

    clean resteasy-reactive

--- resteasy-reactive/src/main/java/org/example/ServerVertxBufferMessageBodyWriter.java
+++ resteasy-reactive/src/main/java/org/example/ServerVertxBufferMessageBodyWriter.java
@@ -6,20 +6,23 @@ import org.jboss.resteasy.reactive.server.spi.ResteasyReactiveResourceInfo;
 import org.jboss.resteasy.reactive.server.spi.ServerMessageBodyWriter;
 import org.jboss.resteasy.reactive.server.spi.ServerRequestContext;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.lang.reflect.Type;
 
 @Provider
 public class ServerVertxBufferMessageBodyWriter extends VertxBufferMessageBodyWriter
         implements ServerMessageBodyWriter<Buffer> {
 
     @Override
-    public boolean isWriteable(Class<?> type, ResteasyReactiveResourceInfo target, MediaType mediaType) {
+    public boolean isWriteable(Class<?> aClass, Type type, ResteasyReactiveResourceInfo resteasyReactiveResourceInfo, MediaType mediaType) {
         return true;
     }
 
     @Override
-    public void writeResponse(Buffer buffer, ServerRequestContext context) {
-        context.serverResponse().end(buffer.getBytes());
+    public void writeResponse(Buffer buffer, Type type, ServerRequestContext serverRequestContext) throws WebApplicationException, IOException {
+        serverRequestContext.serverResponse().end(buffer.getBytes());
     }
 }
