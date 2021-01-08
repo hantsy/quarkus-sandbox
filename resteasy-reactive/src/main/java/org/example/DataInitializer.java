6a58428d2951df6a9c8a219395b1ddee5ca106fc
Hantsy Bai <hantsy@gmail.com>
2021-01-08 18:18:08 +0800

    update to Quarkus 1.11.0.CR1

--- resteasy-reactive/src/main/java/org/example/DataInitializer.java
+++ resteasy-reactive/src/main/java/org/example/DataInitializer.java
@@ -17,22 +17,23 @@ public class DataInitializer {
     @Inject
     PostRepository posts;
 
+    // There is an issue call reactive opertions in the blocking codes.
+    // see: https://github.com/quarkusio/quarkus/issues/14044
     public void onStart(@Observes StartupEvent ev) {
         LOGGER.info("The application is starting...");
 
         Post first = Post.of("Hello Quarkus", "My first post of Quarkus");
         Post second = Post.of("Hello Again, Quarkus", "My second post of Quarkus");
 
-
         this.posts.deleteAll()
+                .onItem().invoke(l -> LOGGER.log(Level.INFO, "deleted {0} posts.", new Object[]{l}))
                 .flatMap(d -> this.posts.persist(List.of(first, second)))
-                .onItem().transformToUni(v -> this.posts.findAll().list())
+                .flatMap(v -> this.posts.findAll().list())
                 .subscribe()
                 .with(
                         rows -> rows.forEach(r -> LOGGER.log(Level.INFO, "data:{0}", r)),
-                        err -> LOGGER.log(Level.SEVERE, "error:{0}", err)
+                        err -> LOGGER.log(Level.SEVERE, "error:{0}", err.toString())
                 );
-
     }
 
     void onStop(@Observes ShutdownEvent ev) {
