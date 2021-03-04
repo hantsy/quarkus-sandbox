8d182f53b124e9aa01a9aa7c1ab50de70011f74b
Hantsy Bai <hantsy@gmail.com>
2021-03-04 18:48:15 +0800

    clean datasource

--- post-service/src/test/java/com/example/H2Profile.java
+++ post-service/src/test/java/com/example/H2Profile.java
@@ -1,13 +1,14 @@
 package com.example;
 
 import io.quarkus.test.junit.QuarkusTestProfile;
-
 import java.util.Map;
 
 public class H2Profile implements QuarkusTestProfile {
+
     @Override
     public Map<String, String> getConfigOverrides() {
         return Map.of(
+                "quarkus.datasource.db-kind", "h2",
                 "quarkus.datasource.jdbc.url", "jdbc:h2:tcp://localhost/mem:test",
                 "quarkus.datasource.jdbc.driver", "org.h2.Driver",
                 "quarkus.hibernate-orm.database.generation", "drop-and-create",
