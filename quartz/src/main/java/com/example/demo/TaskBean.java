4187357d0a915b99f4df27700609bf1b58fc89bc
Hantsy Bai <hantsy@gmail.com>
2021-05-04 20:19:54 +0800

    clean codes.

--- quartz/src/main/java/com/example/demo/TaskBean.java
+++ quartz/src/main/java/com/example/demo/TaskBean.java
@@ -15,3 +15,43 @@ public class TaskBean {
         task.persist();
     }
 }
+
+//@ApplicationScoped
+//public class TaskBean {
+//
+//    @Inject
+//    org.quartz.Scheduler quartz;
+//
+//    void onStart(@Observes StartupEvent event) throws SchedulerException {
+//        JobDetail job = JobBuilder.newJob(MyJob.class)
+//                .withIdentity("myJob", "myGroup")
+//                .build();
+//        Trigger trigger = TriggerBuilder.newTrigger()
+//                .withIdentity("myTrigger", "myGroup")
+//                .startNow()
+//                .withSchedule(
+//                        SimpleScheduleBuilder.simpleSchedule()
+//                                .withIntervalInSeconds(10)
+//                                .repeatForever())
+//                .build();
+//        quartz.scheduleJob(job, trigger);
+//    }
+//
+//    @Transactional
+//    void performTask() {
+//        Task task = new Task();
+//        task.persist();
+//    }
+//
+//    // A new instance of MyJob is created by Quartz for every job execution
+//    public static class MyJob implements Job {
+//
+//        @Inject
+//        TaskBean taskBean;
+//
+//        public void execute(JobExecutionContext context) throws JobExecutionException {
+//            taskBean.performTask();
+//        }
+//
+//    }
+//}
