826631dc9ab1061c5585852106619fff575c74bc
Hantsy Bai <hantsy@gmail.com>
2021-05-07 11:41:08 +0800

    update h2 profile.

--- post-service/src/test/java/com/example/H2Profile.java
+++ post-service/src/test/java/com/example/H2Profile.java
@@ -11,11 +11,11 @@ public class H2Profile implements QuarkusTestProfile {
     @Override
     public Map<String, String> getConfigOverrides() {
         return Map.of(
-                "quarkus.datasource.db-kind", "h2",
-                "quarkus.datasource.jdbc.url", "jdbc:h2:tcp://localhost/mem:test",
-                "quarkus.datasource.jdbc.driver", "org.h2.Driver",
-                "quarkus.hibernate-orm.database.generation", "drop-and-create",
-                "quarkus.hibernate-orm.log.sql", "true"
+//                "quarkus.datasource.db-kind", "h2",
+//                "quarkus.datasource.jdbc.url", "jdbc:h2:tcp://localhost/mem:test",
+//                "quarkus.datasource.jdbc.driver", "org.h2.Driver",
+//                "quarkus.hibernate-orm.database.generation", "drop-and-create",
+//                "quarkus.hibernate-orm.log.sql", "true"
         );
     }
 
