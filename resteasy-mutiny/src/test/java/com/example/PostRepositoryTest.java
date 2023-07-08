e96fa0b876a7e6b4bb790db7f23d5f406c31616f
hantsy <hantsy@gmail.com>
2023-07-08 15:01:15 +0800

    chore: clean codes

---
+++ resteasy-mutiny/src/test/java/com/example/PostRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+//@QuarkusTestResource(H2DatabaseTestResource.class)
+public class PostRepositoryTest {
+
+    @Inject
+    PostRepository postRepository;
+
+    @Test
+    public void testSave() {
+        var assertSubscriber = postRepository.save(Post.of("test", "test"))
+                .invoke(p -> assertThat(p).isNotNull())
+                .subscribe().withSubscriber(UniAssertSubscriber.create());
+        assertSubscriber.awaitItem().assertCompleted();
+    }
+}
