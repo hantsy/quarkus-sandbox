3dc997e10da44e1b629623f02374938ee78cb433
hantsy <hantsy@gmail.com>
2023-07-10 13:49:39 +0800

    chore: clean codes

--- hibernate-reactive/src/main/java/com/example/DataInitializer.java
+++ hibernate-reactive/src/main/java/com/example/DataInitializer.java
@@ -1,56 +1,41 @@
 package com.example;
 
+import io.quarkus.arc.profile.IfBuildProfile;
 import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.Startup;
 import io.quarkus.runtime.StartupEvent;
-import io.vertx.mutiny.pgclient.PgPool;
-import io.vertx.mutiny.sqlclient.Tuple;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ApplicationScoped
+@Startup
+@IfBuildProfile("dev")
 public class DataInitializer {
-    private final static Logger LOGGER = Logger.getLogger(DataInitializer.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DataInitializer.class.getName());
 
     @Inject
-    PgPool client;
+    PostRepository posts;
 
     public void onStart(@Observes StartupEvent ev) {
         LOGGER.info("The application is starting...");
 
-        Tuple first = Tuple.of("Hello Quarkus", "My first post of Quarkus");
-        Tuple second = Tuple.of("Hello Again, Quarkus", "My second post of Quarkus");
+        Post first = Post.of("Hello Quarkus", "My first post of Quarkus");
+        Post second = Post.of("Hello Again, Quarkus", "My second post of Quarkus");
 
-        client.query(readInitScript()).execute()
-                .chain(c -> client.query("DELETE FROM posts").execute())
-                .chain(result -> client.preparedQuery("INSERT INTO posts (title, content) VALUES ($1, $2)").executeBatch(List.of(first, second)))
-                .chain(rs -> client.query("SELECT * FROM posts").execute())
+        this.posts.deleteAll()
+                .onItem().invoke(l -> LOGGER.log(Level.INFO, "deleted {0} posts.", new Object[]{l}))
+                .chain(d -> this.posts.save(first))
+                .chain(d -> this.posts.save(second))
+                .chain(v -> this.posts.findAll())
                 .subscribe()
                 .with(
                         rows -> rows.forEach(r -> LOGGER.log(Level.INFO, "data:{0}", r)),
-                        err -> LOGGER.log(Level.SEVERE, "error:{0}", err)
+                        err -> LOGGER.log(Level.SEVERE, "error:{0}", err.toString())
                 );
-
-    }
-
-    private String readInitScript() {
-        try {
-            return Files.readString(Paths.get(this.getClass().getResource("/init.sql").toURI()), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     void onStop(@Observes ShutdownEvent ev) {
