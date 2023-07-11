37b12f4e5f26ccdf82f71c0d0ffc06b8384eb9c4
hantsy <hantsy@gmail.com>
2023-07-11 15:20:17 +0800

    chore: clean codes

--- resteasy-reactive/src/main/java/com/example/DataInitializer.java
+++ resteasy-reactive/src/main/java/com/example/DataInitializer.java
@@ -1,19 +1,19 @@
 package com.example;
 
 import io.quarkus.arc.profile.IfBuildProfile;
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.runtime.ShutdownEvent;
-import io.quarkus.runtime.Startup;
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.vertx.VertxContextSupport;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ApplicationScoped
-@Startup
 @IfBuildProfile("dev")
 @RequiredArgsConstructor
 public class DataInitializer {
@@ -23,21 +23,24 @@ public class DataInitializer {
 
     // There is an issue call reactive operations in the blocking codes.
     // see: https://github.com/quarkusio/quarkus/issues/14044
+    @SneakyThrows
     public void onStart(@Observes StartupEvent ev) {
         LOGGER.info("The application is starting...");
 
         Post first = Post.of("Hello Quarkus", "My first post of Quarkus");
         Post second = Post.of("Hello Again, Quarkus", "My second post of Quarkus");
 
-        this.posts.deleteAll()
-                .onItem().invoke(l -> LOGGER.log(Level.INFO, "deleted {0} posts.", new Object[]{l}))
-                .chain(d -> this.posts.persist(List.of(first, second)))
-                .chain(v -> this.posts.findAll().list())
-                .subscribe()
-                .with(
-                        rows -> rows.forEach(r -> LOGGER.log(Level.INFO, "data:{0}", r)),
-                        err -> LOGGER.log(Level.SEVERE, "error:{0}", err.toString())
-                );
+        var result = VertxContextSupport.subscribeAndAwait(() ->
+                Panache.withTransaction(() ->
+                        posts.deleteAll()
+                                .onItem().invoke(l -> LOGGER.log(Level.INFO, "deleted {0} posts.", l))
+                                .chain(d -> posts.persist(first, second))
+                                .chain(v -> posts.findAll().list())
+                                .invoke(all -> LOGGER.log(Level.INFO, "found  {0} posts.", all.size()))
+                )
+        );
+
+        LOGGER.log(Level.INFO, "The initialized result is: {0}", result);
     }
 
     void onStop(@Observes ShutdownEvent ev) {
