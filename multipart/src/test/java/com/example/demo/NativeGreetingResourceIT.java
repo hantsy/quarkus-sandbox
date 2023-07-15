1766a347b9064f9cb8470a520b065e63de24ca2e
hantsy <hantsy@gmail.com>
2023-07-15 16:19:47 +0800

    chore: clean codes

--- multipart/src/test/java/com/example/demo/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NativeGreetingResourceIT extends MultipartResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- quartz/src/test/java/com/example/demo/NativeTaskResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NativeTaskResourceIT extends TaskResourceTest {
-
-    // Execute the same tests but in native mode.
-}
