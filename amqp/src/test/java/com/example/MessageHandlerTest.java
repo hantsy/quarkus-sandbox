a4c51d445fd08c0639a660d1367613d0d0f5e436
hantsy <hantsy@gmail.com>
2023-07-18 19:04:27 +0800

    chore: clean codes

--- amqp/src/test/java/com/example/MessageHandlerTest.java
+++ amqp/src/test/java/com/example/MessageHandlerTest.java
@@ -8,10 +8,8 @@ import jakarta.enterprise.inject.Any;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.spy;
 
 @QuarkusTest
 class MessageHandlerTest {
@@ -29,19 +27,15 @@ class MessageHandlerTest {
 
     @Test
     void receive() {
-        MessageHandler handlerSpy = spy(handler);
-        var inputCaptor = ArgumentCaptor.forClass(String.class);
-
         InMemorySource<String> messages = connector.source("messages");
         InMemorySink<String> sink = connector.sink("send");
+        InMemorySink<Message> dataStream = connector.sink("data-stream");
+
 
         handler.send("hello");
         assertThat(sink.received().get(0).getPayload()).isEqualTo("hello");
 
-        messages.send("hello");
-
-        var outputResult = handlerSpy.receive(inputCaptor.capture());
-        assertThat(inputCaptor.getValue()).isEqualTo("hello");
-        assertThat(outputResult.body()).isEqualTo("hello");
+        messages.send("hello-123");
+        assertThat(dataStream.received().get(0).getPayload().body()).isEqualTo("hello-123");
     }
 }
