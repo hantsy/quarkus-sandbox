714b69e8232081d57f9c3325cb9adf888e3c5fec
hantsy <hantsy@gmail.com>
2023-07-23 11:54:24 +0800

    chore: clean codes

---
+++ amqp-rabbitmq/src/main/java/com/example/MessageHandler.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import java.time.Instant;
+
+@ApplicationScoped
+@Slf4j
+public class MessageHandler {
+    @Inject
+    @Channel("send")
+    Emitter<String> emitter;
+
+    public void send(String message) {
+        emitter.send(message);
+    }
+
+    @Incoming("messages")
+    @Outgoing("data-stream")
+    @Broadcast
+    Message receive(String message) {
+        log.info("received: {}", message);
+        return new Message(message, Instant.now());
+    }
+}
