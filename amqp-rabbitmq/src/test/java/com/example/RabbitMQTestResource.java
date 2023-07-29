90b6e7fc64dcb9efc5f760dce96d1b7b82cc122f
hantsy <hantsy@gmail.com>
2023-07-29 14:23:14 +0800

    chore: clean codes

--- amqp-rabbitmq/src/test/java/com/example/RabbitMQTestResource.java
+++ amqp-rabbitmq/src/test/java/com/example/RabbitMQTestResource.java
@@ -5,17 +5,21 @@ import org.testcontainers.containers.Network;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
+import java.time.Duration;
 import java.util.Map;
 
 public class RabbitMQTestResource implements QuarkusTestResourceLifecycleManager {
     static RabbitMQContainer container = new RabbitMQContainer("rabbitmq:3-management-alpine")
             .withNetwork(Network.SHARED)
             .withNetworkAliases("rabbitmq")
-            .withUser("quarkus","quarkus");
+            .withUser("quarkus","quarkus")
+            .withPluginsEnabled("rabbitmq_amqp1_0");
     @Override
     public Map<String, String> start() {
         container.start();
-        Wait.forHealthcheck();
+        Wait.forHealthcheck()
+                .withStartupTimeout(Duration.ofMillis(30000))
+                .waitUntilReady(container);
         return Map.of(
                 "amqp-host", container.getHost(),
                 "amqp-port",container.getFirstMappedPort()+"",
--- rabbitmq/src/test/java/com/example/MessageResourceTest.java
+++ rabbitmq/src/test/java/com/example/MessageResourceTest.java
@@ -9,10 +9,13 @@ import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.sse.SseEventSource;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,19 +37,31 @@ class MessageResourceTest {
         this.client = ClientBuilder.newClient();
     }
 
+    @SneakyThrows
     @Test
     void testSendAndReceiveMessages() {
         LOGGER.log(Level.INFO, "base url: {0}", url);
+
+        var messageReplay = new ArrayList<Message>();
+        var latch = new CountDownLatch(1);
         try (SseEventSource sse = SseEventSource.target(this.client.target(url.toExternalForm()))
-                .reconnectingEvery(2, TimeUnit.SECONDS)
+                //.reconnectingEvery(2, TimeUnit.SECONDS)
                 .build()) {
             sse.register(
                     event -> {
-                        var message = event.readData(Message.class);
-                        assertThat(message.body()).isEqualTo("hello");
+                        LOGGER.log(Level.INFO, "handling event: {0}", event);
+                        var message = event.readData(Message.class, MediaType.APPLICATION_JSON_TYPE);
+                        LOGGER.log(Level.INFO, "reading event data: {0}", message);
+                        messageReplay.add(message);
                     },
-                    error -> LOGGER.log(Level.SEVERE, "error: {}", error),
-                    () -> LOGGER.log(Level.INFO, "done")
+                    error -> {
+                        LOGGER.log(Level.SEVERE, "error: {}", error);
+                        latch.countDown();
+                    },
+                    () -> {
+                        LOGGER.log(Level.INFO, "done");
+                        latch.countDown();
+                    }
             );
             sse.open();
 
@@ -55,6 +70,12 @@ class MessageResourceTest {
                     .post(Entity.entity("hello", MediaType.TEXT_PLAIN_TYPE))) {
                 assertThat(sendMessageResponse.getStatus()).isEqualTo(204);
             }
+
+            latch.await(500, TimeUnit.MILLISECONDS);
         }
+
+        assertThat(messageReplay.size()).isEqualTo(1);
+        assertThat(messageReplay.get(0).body()).isEqualTo("hello");
     }
+
 }
