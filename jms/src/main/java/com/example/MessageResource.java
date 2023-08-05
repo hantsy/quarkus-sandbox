b4f9493a8e1ccab0d4779207b8baf6188bb24c0c
hantsy <hantsy@gmail.com>
2023-08-05 18:34:38 +0800

    chore: clean codes

--- jms/src/main/java/com/example/MessageResource.java
+++ jms/src/main/java/com/example/MessageResource.java
@@ -1,10 +1,13 @@
 package com.example;
 
+import io.smallrye.mutiny.Multi;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
 import org.jboss.resteasy.annotations.SseElementType;
+import org.reactivestreams.FlowAdapters;
+import org.reactivestreams.Publisher;
 
 import java.util.concurrent.Flow;
 
@@ -25,7 +28,9 @@ public class MessageResource {
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @SseElementType(MediaType.APPLICATION_JSON)
-    public Flow.Publisher<Message> stream() {
+    public Multi<Message> stream() {
         return handler.emitterProcessor.toMulti();
+        // see:
+        // return FlowAdapters.toPublisher( handler.emitterProcessor.toMulti());
     }
 }
--- jms/src/test/java/com/example/ArtemisTestResource.java
+++ jms/src/test/java/com/example/ArtemisTestResource.java
@@ -2,26 +2,24 @@ package com.example;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
 
-import java.time.Duration;
 import java.util.Map;
 
 public class ArtemisTestResource implements QuarkusTestResourceLifecycleManager {
 
     private static final GenericContainer container = new GenericContainer("quay.io/artemiscloud/activemq-artemis-broker")
-            .withExposedPorts(5672, 61616)
+            .withExposedPorts(5672)
             .withEnv("AMQ_USER", "quarkus")
-            .withEnv("AMQ_PASSWORD", "quarkus")
-            .waitingFor(new HostPortWaitStrategy()
-                    .withStartupTimeout(Duration.ofMillis(5000))
-            );
+            .withEnv("AMQ_PASSWORD", "quarkus");
+//            .waitingFor(new HostPortWaitStrategy()
+//                    .withStartupTimeout(Duration.ofMillis(5000))
+//            );
 
     @Override
     public Map<String, String> start() {
         container.start();
         return Map.of(
-                "quarkus.qpid-jms.url", "amqp://localhost:5672",
+                "quarkus.qpid-jms.url", "amqp://localhost:" + container.getFirstMappedPort(),
                 "quarkus.qpid-jms.username", "quarkus",
                 "quarkus.qpid-jms.password", "quarkus"
         );
