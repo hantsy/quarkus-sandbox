a3b9651a8065e961817ffae379b8e5cc01209e37
hantsy <hantsy@gmail.com>
2023-08-07 13:44:03 +0800

    chore: clean codes

--- jms/src/main/java/com/example/JmsResources.java
+++ jms/src/main/java/com/example/JmsResources.java
@@ -2,18 +2,26 @@ package com.example;
 
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSContext;
 import jakarta.jms.Queue;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class JmsResources {
+    private static final Logger LOGGER = Logger.getLogger(JmsResources.class.getName());
     private final static String HELLO_QUEUE = "HELLO_QUEUE";
 
     @Produces
     JMSContext jmsContext(ConnectionFactory connectionFactory) {
         return connectionFactory.createContext();
+    }
+
+    void closeJmsContext(@Disposes JMSContext jmsContext) {
+        LOGGER.log(Logger.Level.INFO, "disposing JMSContext...");
+        jmsContext.close();
     }
 
     @Produces
